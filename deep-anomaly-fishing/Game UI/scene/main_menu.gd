5aabd672d652ecf44a46de2a8ee9ac9af1919ba6
RaftFeed <rafidharsyahh@gmail.com>
2025-09-13 16:22:00 +0700

    initial commit

---
+++ deep-anomaly-fishing/Game UI/scene/main_menu.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+func _on_start_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_how_to_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credit_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
